79f077c6877bc8d0f992c4b8729078ae53cae47a
Sundar_Charan <sundarcharan5@gmail.com>
2025-03-11 11:51:42 +0530

    init file

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <!-- EMail -->
+    <!-- USer Name -->
+    <!-- City -->
+     <!-- Password -->
+     <!-- confirm Password -->
+      <form>
+        <div>
+            <label>Email</label><br>
+            <input type="email" name="email" id="email"/>
+        </div>
+        <div>
+            <label>User name</label><br>
+            <input type="text" name="username" id="username"/>
+        </div>
+        <div>
+            <label>City</label><br>
+            <input type="text" name="city" id="city"/>
+        </div>
+        <div>
+            <label>Password</label><br>
+            <input type="password" name="password" id="password"/>
+        </div>
+        <div>
+            <label>Confirm Password</label><br>
+            <input type="password" name="confirmPassword" id="confirmPassword"/>
+        </div>
+        <div>
+            
+
+        <input type="submit" name="register" id="reg" />
+        </div>
+        </form>
+</body>
+</html>
